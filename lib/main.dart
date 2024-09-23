deae13af8ac7361658454ca4c193e7ffb16149a3
suhaibusman <muhammad.suhaib.usman@blocship.io>
2024-09-24 02:01:38 +0500

    Added logo

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Doctor Appointment App',
       theme: ThemeData(
         // This is the theme of your application.
         //
